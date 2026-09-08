c18462ad9eaa06cfd35ad183082867a123587a63
KinwipeCode <kinwipecode@users.noreply.github.com>
2026-09-08 22:49:55 +0200

    feat: publish/update via Git-Publisher

--- components/Ribbon.tsx
+++ components/Ribbon.tsx
@@ -250,7 +250,7 @@ export function Ribbon({ onOpenFile, activeDocId }: RibbonProps) {
             }}
             title="PDF Editor Pro Version"
           >
-            v1.0.2
+            v1.0.3
           </span>
         </div>
       </div>
